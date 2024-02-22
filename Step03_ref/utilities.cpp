c3dc947ac362e0fa92306e2a6e84cff4e50a1366
Nikolay S <sovetov.k@gmail.com>
2024-02-22 16:50:17 +0300

    initialization

---
+++ Step03_ref/utilities.cpp
@@ -0,0 +1,15 @@
+#include "StdAfx.h"
+#include "utilities.h"
+
+void UtilityCreator::CreateEmployee() const {
+	
+
+}
+
+void UtilityCreator::CreateLayer(const ACHAR* layerName) const {
+
+}
+
+
+
+
---
+++ Step03_ref/utilities.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "tchar.h"
+
+class UtilityCreator {
+
+public:
+	void CreateEmployee() const;
+	void CreateLayer(const ACHAR* layerName) const;
+};
+
+
+
